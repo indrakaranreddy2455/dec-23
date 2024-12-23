aec21e08a73cfbd33816afc0c8722b131ec5faae
indrakaranreddy <you@example.com>
2024-12-23 12:16:19 +0530

    first commit

---
+++ Studentmarks.java
@@ -0,0 +1,44 @@
+
+public class Studentmarks {
+
+	public static void main(String[] args) {
+				
+				
+				
+				/// Student s1 = new Student(100,"S1",56,66,78,56000);
+				
+				/*
+				 * 
+				 * 
+				 * Student -- class
+				 * 
+				 * id
+				 * name
+				 * 3 sub marks
+				 * fee
+				 * 
+				 * 50 students
+				 * 
+				 * 1. Total Fee Due of All
+				 * 
+				 * 2. Total pass
+				 * 
+				 * 3.Total fail
+				 * 4. total only in one sub
+				 * 5. Grace of lesser 3 marks
+				 * 
+				 * 
+				 * 
+				 * 
+				 * 
+				 * 
+				 * 
+				 * 
+				 * 
+				 * 
+				 * 
+				 */
+	
+	}
+
+}
